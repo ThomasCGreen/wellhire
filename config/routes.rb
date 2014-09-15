f25e798b82429554794398e4f26523255352aef7
Thomas Green <TomG@AccelHomeBuyers.com>
2014-09-15 13:34:14 -0600

    added css items

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'opportunities#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'employer/opportunities' => 'opportunities#view'
+  get 'employer/opportunities' => 'opportunities#view', as: :view
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
